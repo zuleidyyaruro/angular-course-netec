b58346469bad47a9eabbba889cf1998f280e4ec1
Zuleidy Yaruro <zvyaruro@gmail.com>
2025-06-16 10:15:13 -0600

    services, components for add and delete

--- dogsapp/src/app/components/dog-table/dog-table.component.ts
+++ dogsapp/src/app/components/dog-table/dog-table.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { DogService } from '../../services/dog.service';
+import { Dog } from '../../interfaces/dog.interface';
+import { MatCardModule } from '@angular/material/card';
+
+import { RouterLink } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-dog-table',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, RouterLink],
   templateUrl: './dog-table.component.html',
   styleUrl: './dog-table.component.css'
 })
-export class DogTableComponent {
+export class DogTableComponent implements OnInit {
+
+  private service: DogService = inject(DogService);
+  dogs = signal<Dog[]>([]);
+
+  ngOnInit() {
+    this.service.getDogs().subscribe({
+      next: values => this.dogs.set(values),
+      error: error => console.log(error)
+    })
+  }
+
+  deleteDog(id: number) {
+    this.service.deleteDog(id).subscribe({
+      error: error => { console.log(error) }
+    })
+  }
 
 }
--- dogsapp/src/app/components/dogs-form/dogs-form.component.ts
+++ dogsapp/src/app/components/dogs-form/dogs-form.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DogService } from '../../services/dog.service';
+import { Dog } from '../../interfaces/dog.interface';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dogs-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
   templateUrl: './dogs-form.component.html',
   styleUrl: './dogs-form.component.css'
 })
 export class DogsFormComponent {
 
+  private service: DogService = inject(DogService);
+  private build = inject(FormBuilder);
+  private router = inject(Router)
+
+  form = this.build.group({
+    breed: ['', [Validators.required, Validators.maxLength(20), Validators.pattern(/^[a-z,A-Z,\s]/)]],
+    description: ['', [Validators.required, Validators.maxLength(200), Validators.pattern(/^[a-z,A-Z,\s]/)]],
+    urlImage: ['', [Validators.pattern(/(https?:\/\/.*\.(?:png|jpg|jpeg))/i), Validators.required, Validators.maxLength(200)]]
+
+  })
+
+  submit() {
+    let breed = this.form.controls.breed.value!;
+    let description = this.form.controls.description.value!;
+    let urlImage = this.form.controls.urlImage.value!;
+
+    let dog: Dog = {
+      id: 0,
+      breed,
+      description,
+      urlImage
+    }
+
+    this.service.insertDog(dog).pipe(
+      take(1)).subscribe({
+        next: value => {
+          this.router.navigate(['dogs'])
+        },
+        error: error => console.log(error)
+      })
+  }
 }
